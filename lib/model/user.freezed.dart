dce770d7258a9c6a6a306f3c99373c59c8391b41
nusret35 <96892300+nusret35@users.noreply.github.com>
2022-06-02 01:46:37 +0300

    build grade is different and personal

---
+++ lib/model/user.freezed.dart
@@ -0,0 +1,222 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'user.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+AppUser _$AppUserFromJson(Map<String, dynamic> json) {
+  return _AppUser.fromJson(json);
+}
+
+/// @nodoc
+class _$AppUserTearOff {
+  const _$AppUserTearOff();
+
+  _AppUser call(
+      {required String email,
+      required String password,
+      required String name,
+      required String surname}) {
+    return _AppUser(
+      email: email,
+      password: password,
+      name: name,
+      surname: surname,
+    );
+  }
+
+  AppUser fromJson(Map<String, Object?> json) {
+    return AppUser.fromJson(json);
+  }
+}
+
+/// @nodoc
+const $AppUser = _$AppUserTearOff();
+
+/// @nodoc
+mixin _$AppUser {
+  String get email => throw _privateConstructorUsedError;
+  String get password => throw _privateConstructorUsedError;
+  String get name => throw _privateConstructorUsedError;
+  String get surname => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $AppUserCopyWith<AppUser> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $AppUserCopyWith<$Res> {
+  factory $AppUserCopyWith(AppUser value, $Res Function(AppUser) then) =
+      _$AppUserCopyWithImpl<$Res>;
+  $Res call({String email, String password, String name, String surname});
+}
+
+/// @nodoc
+class _$AppUserCopyWithImpl<$Res> implements $AppUserCopyWith<$Res> {
+  _$AppUserCopyWithImpl(this._value, this._then);
+
+  final AppUser _value;
+  // ignore: unused_field
+  final $Res Function(AppUser) _then;
+
+  @override
+  $Res call({
+    Object? email = freezed,
+    Object? password = freezed,
+    Object? name = freezed,
+    Object? surname = freezed,
+  }) {
+    return _then(_value.copyWith(
+      email: email == freezed
+          ? _value.email
+          : email // ignore: cast_nullable_to_non_nullable
+              as String,
+      password: password == freezed
+          ? _value.password
+          : password // ignore: cast_nullable_to_non_nullable
+              as String,
+      name: name == freezed
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      surname: surname == freezed
+          ? _value.surname
+          : surname // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$AppUserCopyWith<$Res> implements $AppUserCopyWith<$Res> {
+  factory _$AppUserCopyWith(_AppUser value, $Res Function(_AppUser) then) =
+      __$AppUserCopyWithImpl<$Res>;
+  @override
+  $Res call({String email, String password, String name, String surname});
+}
+
+/// @nodoc
+class __$AppUserCopyWithImpl<$Res> extends _$AppUserCopyWithImpl<$Res>
+    implements _$AppUserCopyWith<$Res> {
+  __$AppUserCopyWithImpl(_AppUser _value, $Res Function(_AppUser) _then)
+      : super(_value, (v) => _then(v as _AppUser));
+
+  @override
+  _AppUser get _value => super._value as _AppUser;
+
+  @override
+  $Res call({
+    Object? email = freezed,
+    Object? password = freezed,
+    Object? name = freezed,
+    Object? surname = freezed,
+  }) {
+    return _then(_AppUser(
+      email: email == freezed
+          ? _value.email
+          : email // ignore: cast_nullable_to_non_nullable
+              as String,
+      password: password == freezed
+          ? _value.password
+          : password // ignore: cast_nullable_to_non_nullable
+              as String,
+      name: name == freezed
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      surname: surname == freezed
+          ? _value.surname
+          : surname // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_AppUser implements _AppUser {
+  const _$_AppUser(
+      {required this.email,
+      required this.password,
+      required this.name,
+      required this.surname});
+
+  factory _$_AppUser.fromJson(Map<String, dynamic> json) =>
+      _$$_AppUserFromJson(json);
+
+  @override
+  final String email;
+  @override
+  final String password;
+  @override
+  final String name;
+  @override
+  final String surname;
+
+  @override
+  String toString() {
+    return 'AppUser(email: $email, password: $password, name: $name, surname: $surname)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _AppUser &&
+            const DeepCollectionEquality().equals(other.email, email) &&
+            const DeepCollectionEquality().equals(other.password, password) &&
+            const DeepCollectionEquality().equals(other.name, name) &&
+            const DeepCollectionEquality().equals(other.surname, surname));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(email),
+      const DeepCollectionEquality().hash(password),
+      const DeepCollectionEquality().hash(name),
+      const DeepCollectionEquality().hash(surname));
+
+  @JsonKey(ignore: true)
+  @override
+  _$AppUserCopyWith<_AppUser> get copyWith =>
+      __$AppUserCopyWithImpl<_AppUser>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_AppUserToJson(this);
+  }
+}
+
+abstract class _AppUser implements AppUser {
+  const factory _AppUser(
+      {required String email,
+      required String password,
+      required String name,
+      required String surname}) = _$_AppUser;
+
+  factory _AppUser.fromJson(Map<String, dynamic> json) = _$_AppUser.fromJson;
+
+  @override
+  String get email;
+  @override
+  String get password;
+  @override
+  String get name;
+  @override
+  String get surname;
+  @override
+  @JsonKey(ignore: true)
+  _$AppUserCopyWith<_AppUser> get copyWith =>
+      throw _privateConstructorUsedError;
+}
---
+++ lib/model/user.g.dart
@@ -0,0 +1,22 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_AppUser _$$_AppUserFromJson(Map<String, dynamic> json) => _$_AppUser(
+      email: json['email'] as String,
+      password: json['password'] as String,
+      name: json['name'] as String,
+      surname: json['surname'] as String,
+    );
+
+Map<String, dynamic> _$$_AppUserToJson(_$_AppUser instance) =>
+    <String, dynamic>{
+      'email': instance.email,
+      'password': instance.password,
+      'name': instance.name,
+      'surname': instance.surname,
+    };
