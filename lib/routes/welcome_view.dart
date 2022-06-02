cdc285ecfcaf53038860fce7b6a888be4a4a6e75
nusret35 <96892300+nusret35@users.noreply.github.com>
2022-06-02 13:39:10 +0300

    some changes including build.gradle

--- lib/routes/welcome_view.dart
+++ lib/routes/welcome_view.dart
@@ -11,74 +11,76 @@ class WelcomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.whiteTextColor,
-      body: SafeArea(
-        maintainBottomViewPadding: false,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.all(0),
-                child: Column(
-                  children: [
-                    Image.asset("assets/welcome_view_top_image.png"),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          maintainBottomViewPadding: false,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(0),
+                  child: Column(
+                    children: [
+                      Image.asset("assets/welcome_view_top_image.png"),
 
-                    Row(
-                      children: [
-                        Padding(padding: EdgeInsets.fromLTRB(130, 150, 0, 0)),
-                        RichText(
-                          text: TextSpan(
+                      Row(
+                        children: [
+                          Padding(padding: EdgeInsets.fromLTRB(130, 150, 0, 0)),
+                          RichText(
+                            text: TextSpan(
 
-                            text:"Welcome to",
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: AppColors.textColor,
-                              fontWeight: FontWeight.w600,
+                              text:"Welcome to",
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: AppColors.textColor,
+                                fontWeight: FontWeight.w600,
 
+                              ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                    Image(image: AssetImage('assets/logo.png'),
-                    ),
-                    Column(
+                        ],
+                      ),
+                      Image(image: AssetImage('assets/logo.png'),
+                      ),
+                      Column(
 
-                      children: <Widget>[
-                        Padding(padding: EdgeInsets.fromLTRB(10, 50, 0, 0)),
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(29),
-                          child: FlatButton(
-                            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 80),
-                            color: AppColors.primary,
-                            onPressed: (){
-                              Navigator.push(context, MaterialPageRoute(builder: (context) => LoginView()));
-                            },
-                            child: Text("LOGIN",style: TextStyle(color: AppColors.whiteTextColor)),
+                        children: <Widget>[
+                          Padding(padding: EdgeInsets.fromLTRB(10, 50, 0, 0)),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(29),
+                            child: FlatButton(
+                              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 80),
+                              color: AppColors.primary,
+                              onPressed: (){
+                                Navigator.push(context, MaterialPageRoute(builder: (context) => LoginView()));
+                              },
+                              child: Text("LOGIN",style: TextStyle(color: AppColors.whiteTextColor)),
+                            ),
                           ),
-                        ),
-                        SizedBox(height: 10,),
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(29),
-                          child: FlatButton(
-                            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 75),
-                            color: AppColors.primary,
-                            onPressed: (){
-                              Navigator.push(context, MaterialPageRoute(builder: (context) => SignUp()));
-                            },
-                            child: Text("SIGN UP",style: TextStyle(color: AppColors.whiteTextColor)),
+                          SizedBox(height: 10,),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(29),
+                            child: FlatButton(
+                              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 75),
+                              color: AppColors.primary,
+                              onPressed: (){
+                                Navigator.push(context, MaterialPageRoute(builder: (context) => SignUp()));
+                              },
+                              child: Text("SIGN UP",style: TextStyle(color: AppColors.whiteTextColor)),
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
+                        ],
+                      ),
 
 
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
 
+          ),
         ),
       ),
     );
